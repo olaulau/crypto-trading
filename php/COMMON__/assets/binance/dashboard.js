9d4d52e312b978a1581dc8f8d265b59ed87580f4
olaulau <olaulau@gmail.com>
2026-01-26 00:30:24 +0100

    dashboard : auto refresh

--- php/COMMON__/assets/binance/dashboard.js
+++ php/COMMON__/assets/binance/dashboard.js
@@ -1,8 +1,35 @@
 $(function() {
 	
 	// hide dust checkbox
-	$("#hide_dust").on("change", function(){
+	$("#hide_dust").on("change", function() {
 		$(".dust").toggleClass("visually-hidden");
 	});
+	
+	
+	// auto refresh
+	let refreshTimeout
+	function auto_refresh () {
+		var auto_refresh = $("#auto_refresh").prop("checked");
+		if (auto_refresh === true) {
+			refreshTimeout = setTimeout(function () {
+					location.reload();
+				},
+				5 * 60 * 1000 // 5 minutes
+			);
+		}
+		else {
+			if (typeof refreshTimeout !== 'undefined') {
+				clearTimeout(refreshTimeout);
+			}
+		}
+	}
+	
+	$("#auto_refresh").on("change", function() {
+		auto_refresh();
+	});
+	
+	auto_refresh();
+	
+	
 	
 });
