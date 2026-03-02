6e8f30e36714cb1b1b6c62b72fd7ad888669d6e5
laulau <olaulau@gmail.com>
2026-03-02 13:57:13 +0100

    WiP : test ws uds as watchdog

--- php/COMMON__/ctrl/WatchdogCtrl.php
+++ php/COMMON__/ctrl/WatchdogCtrl.php
@@ -32,7 +32,7 @@ class WatchdogCtrl extends Ctrl
 	public static function watchdogRun () : void
 	{
 		$w = new Watchdog ("global", 60, "php index.php watchdog global script >> tmp/log/watchdog.log 2>&1");
-		$w->runCached();
+		$w->runCached(); #TODO use $f3->alias("") for command;
 	}
 	
 	public static function watchdogRunGET (Base $f3, $url, $controler) : void
@@ -58,18 +58,24 @@ class WatchdogCtrl extends Ctrl
 	public static function watchdogGlobalScript () : void
 	{
 		CliCtrl::prepareCli ();
+
+		$f3 = Base::instance();
 		
 		echo "[" . (new DateTime)->format(Stuff::datetime_sql_format) . "] : watchdogScript ()" . PHP_EOL;
 		
-		$w = new Watchdog ("global");
+		$watchdog_global = new Watchdog ("global");
 		
 		# while true loop
 		while (1 === 1) {
 			# store last update into db
-			$w->markAsUpdated ();
+			$watchdog_global->markAsUpdated ();
 			
 			#TODO do stuff
 			echo "[" . (new DateTime)->format(Stuff::datetime_sql_format) . "] : loop" . PHP_EOL;
+
+			$watchdog_uds = new Watchdog ("uds", 60, "php index.php " . $f3->alias("wsUds") . " >> tmp/log/ws_uds.log 2>&1");
+			$watchdog_uds->runCached(); #TODO test !
+			#TODO add to watchdog page
 			
 			# sleep 5
 			sleep (5);
